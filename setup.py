10ee7e9ea995ed1ef7e670fcdedf43e39a67911f
Paul Kim <hki34@uclive.ac.nz>
2017-04-04 16:35:08 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from codecs import open
 from os import path
+import sys
 
 from setuptools import setup, find_packages
 
@@ -8,6 +9,10 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+if sys.version_info[0] < 3:
+    req = ['networkx', 'tabulate', 'lxml', 'future', 'statistics']
+else:
+    req = ['networkx', 'labulate', 'lmxl', 'future']
 
 setup(
         name = 'sample',
@@ -18,5 +23,5 @@ setup(
         author_email = 'hki34@uclive.ac.nz',
         keywords = 'security analysis framework',
         packages = find_packages(),
-        install_requires=['networkx', 'tabulate', 'lxml'],
+        install_requires=req,
 )
