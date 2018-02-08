cd527a17a3cad5d715986e6d5c818eb2c8a10984
Hyunjin Kim <hki34@uclive.ac.nz>
2018-02-08 13:22:47 +1300

    Update bayesian test

--- tests/bayesian_test.py
+++ tests/bayesian_test.py
@@ -20,18 +20,12 @@ def test_bayesian():
         vulnerability1 = hm.Vulnerability('CVE-0000', values={
             'risk': 10,
             'cost': 4,
-            'probability': 0.5,
+            'probability': 0.9,
             'impact': 12
-        })
-        vulnerability2 = hm.Vulnerability('CVE-0001', values={
-            'risk': 1,
-            'cost': 5,
-            'probability': 0.2,
-            'impact': 2
         })
         # basic_at creates just one OR gate and puts all vulnerabilites
         # the children nodes
-        host.lower_layer.basic_at([vulnerability1, vulnerability2])
+        host.lower_layer.basic_at([vulnerability1])
 
     # Now we will create an Attacker. This is not a physical node but it exists to describe
     # the potential entry points of attackers.
@@ -41,19 +35,21 @@ def test_bayesian():
     # here h[0] refers to the top layer
     # add_edge(A,B) creates a uni-directional from A -> B.
     h[0].add_edge(attacker, hosts[0])
-    h[0].add_edge(hosts[0], hosts[3])
-    h[0].add_edge(hosts[1], hosts[0])
+    h[0].add_edge(hosts[0], hosts[1])
     h[0].add_edge(hosts[0], hosts[2])
-    h[0].add_edge(hosts[3], hosts[4])
-    h[0].add_edge(hosts[3], hosts[2])
+    h[0].add_edge(hosts[1], hosts[3])
+    h[0].add_edge(hosts[2], hosts[3])
+    h[0].add_edge(hosts[1], hosts[2])
 
     # Now we set the attacker and target
     h[0].source = attacker
-    h[0].target = hosts[4]
+    h[0].target = hosts[3]
     h.flowup()
-    bag = h.bayesian_method()
-    for node in h[0]:
-        print(f'{node} probability: {node.probability}')
-    print(bag)
+    result = h.bayesian_method()
+    epsilon = 1e-4
+    assert abs(result['total'] - 0.8019) < epsilon
+    assert abs(result['total_ac'] - 13.0491) < epsilon
+    assert abs(result['ag_risk'] - 37.5435) < epsilon
+    assert abs(result['roa'] - 9.9873) < epsilon
 
-test_bayesian()
+
